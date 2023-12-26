a58b09fd5d00c0f69b2274552769824d8dc0a17b
Andrei Ilie <andrei.ilie@bolt.eu>
2023-12-26 19:27:07 +0200

    Update __init__.py

--- robustcheck/__init__.py
+++ robustcheck/__init__.py
@@ -1,4 +1,5 @@
-from robustcheck.types.EvoStrategy import *
 from .EvoStrategyUniformUntargeted import *
+from .RobustnessCheck import *
+from .EpsilonGreedyUntargeted import *
 
 __version__ = "1.0.3"
